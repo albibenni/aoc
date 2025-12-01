80a6274ad05ffec4a626a32c74da8c83f64fbfdf
Alberto Benatti <albi.benni8@gmail.com>
2025-12-01 12:14:19 +0100

    very rusty

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 ![image](https://github.com/user-attachments/assets/fbc76f9d-15f8-4151-a74b-4828ee3d777c)
-
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod advent2025;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, AOC!");
 }
