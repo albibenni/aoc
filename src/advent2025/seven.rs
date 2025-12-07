8975b927f8913c978a7ffb65e8e0af66b8a29c7c
Alberto Benatti <albi.benni8@gmail.com>
2025-12-07 17:56:12 +0100

    part2

--- src/advent2025/seven.rs
+++ src/advent2025/seven.rs
@@ -1,5 +1,9 @@
 #![allow(dead_code)]
-use std::{char, collections::HashSet, io::Read};
+use std::{
+    char,
+    collections::{HashMap, HashSet},
+    io::Read,
+};
 
 pub fn sol7() -> (i64, i64) {
     //FILE HANDLING YEEY
@@ -13,8 +17,8 @@ pub fn sol7() -> (i64, i64) {
 
     let mut splitter_set: HashSet<String> = HashSet::new();
 
-    // Convert all lines to Vec<Vec<char>> so we can modify them
     let mut grid: Vec<Vec<char>> = lines.iter().map(|line| line.chars().collect()).collect();
+    let mut s_index: usize = 0;
 
     for i in 0..grid.len() - 1 {
         let line_arr = grid[i].clone();
@@ -23,6 +27,7 @@ pub fn sol7() -> (i64, i64) {
             let value = line_arr[j];
             if value == 'S' {
                 next_line[j] = '|';
+                s_index = j;
             } else if value == '|' {
                 if next_line[j] == '^' {
                     let splitter_key = format!("{},{}", i, j);
@@ -43,5 +48,38 @@ pub fn sol7() -> (i64, i64) {
     }
     let first_res = splitter_set.len() as i64;
 
-    return (first_res, 2);
+    //2nd part
+
+    let grid2: Vec<Vec<char>> = lines.iter().map(|line| line.chars().collect()).collect();
+    let mut current_row: HashMap<usize, usize> = HashMap::new();
+    current_row.insert(s_index, 1);
+
+    for i in 0..grid.len() - 1 {
+        let line_arr = grid2[i + 1].clone();
+        let mut next_line: HashMap<usize, usize> = HashMap::new();
+        for (col_n, sum) in current_row {
+            println!("{},  {}", col_n, sum);
+            let value = line_arr[col_n];
+            if value == '^' {
+                if col_n > 0 {
+                    let prev_sum = *next_line.get(&(col_n - 1)).unwrap_or(&0);
+                    next_line.insert(col_n - 1, prev_sum + sum);
+                }
+                if col_n + 1 < grid[i].len() {
+                    let prev_sum = *next_line.get(&(col_n + 1)).unwrap_or(&0);
+                    next_line.insert(col_n + 1, prev_sum + sum);
+                }
+            } else {
+                let prev_sum = *next_line.get(&col_n).unwrap_or(&0);
+                next_line.insert(col_n, prev_sum + sum);
+            }
+        }
+        current_row = next_line;
+    }
+
+    let mut total: usize = 0;
+    for v in current_row.values() {
+        total += v;
+    }
+    return (first_res, total as i64);
 }
